94dc8e07fecfeee5a54ec534f6336ef343485f81
Ferpm7 <145620501+Ferpm7@users.noreply.github.com>
2023-11-12 21:56:52 -0600

    Add files via upload

--- Operacion.c
+++ Operacion.c
@@ -2,12 +2,7 @@
 #include <stdlib.h>
 #include <math.h>
 
-int suma(int a, int b){
-return a+b;
-}
-int resta(int a, int b){
-return a-b;
-}
+
 
 int main(){
 int num1 = 12;
@@ -16,7 +11,6 @@ int op1 = suma(num1,num2);
 printf("%i",op1);
 int op2 = resta(num1,num2);
 printf("%i",op2);
-int op3 = multi(num1,num2);
-printf("%i",op3);
+
 return 0;
 }
